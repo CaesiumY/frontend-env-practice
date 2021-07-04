ff04e10b6fb2a6e6eabf3b57ea0f4d6ce28274d5
caesiumy <dbs2636@gmail.com>
2021-07-04 22:03:09 +0900

    Add loader

--- 0-tutorials/src/app.js
+++ 0-tutorials/src/app.js
@@ -1,3 +1,13 @@
-import * as math from "./math";
+import "./style.css";
+import bonobono from "./assets/bonobono.jpg";
 
-console.log(math.sum(1, 2));
+console.log("loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.createElement("img");
+  img.src = `${bonobono}`;
+
+  document.body.appendChild(img);
+
+  console.log("img", img);
+});
--- 0-tutorials/webpack.config.js
+++ 0-tutorials/webpack.config.js
@@ -11,4 +11,21 @@ module.exports = {
     path: path.resolve("./dist"),
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+          limit: 10000,
+        },
+      },
+    ],
+  },
 };
