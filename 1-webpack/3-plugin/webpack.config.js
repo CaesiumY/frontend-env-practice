52e4eff8caa4584f1c89191a41dceb27dfe421a5
caesiumy <dbs2636@gmail.com>
2021-07-06 22:53:09 +0900

    Complete 1-3 mission

--- 1-webpack/3-plugin/webpack.config.js
+++ 1-webpack/3-plugin/webpack.config.js
@@ -1,30 +1,39 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist")
+    path: path.resolve("./dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          process.env.NODE_ENV === "development"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
-    ]
-  }
+          limit: 10000, // 10Kb
+        },
+      },
+    ],
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
@@ -32,4 +41,22 @@ module.exports = {
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */
+  plugins: [
+    new webpack.BannerPlugin(`Build Date: ${new Date().toLocaleString()}`),
+    new webpack.DefinePlugin({}),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env:
+          process.env.NODE_ENV === "development" ? "development" : "production",
+      },
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+      hash: true,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
 };
