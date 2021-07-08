705bb9bc0022e3e1ffb692ed0fd6fe9bcb66d904
caesiumy <dbs2636@gmail.com>
2021-07-08 23:03:03 +0900

    Add babel readymade plugins

---
+++ 0-tutorials/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-block-scoping",
+    "@babel/plugin-transform-strict-mode",
+  ],
+  presets: [],
+};
