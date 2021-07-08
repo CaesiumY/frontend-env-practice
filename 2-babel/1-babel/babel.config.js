a307e19c7851a72292579eeae52c6f652cd2544c
caesiumy <dbs2636@gmail.com>
2021-07-08 23:39:24 +0900

    Complete 2-1 mission

--- 2-babel/1-babel/babel.config.js
+++ 2-babel/1-babel/babel.config.js
@@ -1,3 +1,19 @@
 /**
  * TODO: IE11에서도 동작하는 자바스크립트로 변환할수 있게 바벨을 구성하세요.
  */
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          ie: "11",
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: "2",
+        },
+      },
+    ],
+  ],
+};
--- 2-babel/1-babel/webpack.config.js
+++ 2-babel/1-babel/webpack.config.js
@@ -10,11 +10,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist")
+    path: path.resolve("./dist"),
   },
   module: {
     rules: [
@@ -24,43 +24,48 @@ module.exports = {
           process.env.NODE_ENV === "production"
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
             : "style-loader", // 개발 환경
-          "css-loader"
-        ]
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
+          limit: 10000, // 10Kb
+        },
+      },
       /**
        * TODO: babel-loader를 구성해 보세요.
        */
-    ]
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
       },
       minify:
         process.env.NODE_ENV === "production"
           ? {
               collapseWhitespace: true, // 빈칸 제거
-              removeComments: true // 주석 제거
+              removeComments: true, // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production"
+      hash: process.env.NODE_ENV === "production",
     }),
     new CleanWebpackPlugin(),
     ...(process.env.NODE_ENV === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : [])
-  ]
+      : []),
+  ],
 };
