a5c30ba3df13f053819b93f82f941e5f3e58429e
caesiumy <dbs2636@gmail.com>
2021-07-09 00:59:25 +0900

    Start 3-1 mission

---
+++ 3-lint/1-eslint/src/app.js
@@ -0,0 +1,14 @@
+/**
+ * TODO eslint가 중복 세미콜론을 제거합니다.
+ */
+
+
+import MainController from './controllers/MainController.js'
+
+import './app.scss'
+
+const foo = ''
+
+document.addEventListener("DOMContentLoaded", ()=>{
+new MainController();;;; 
+})
