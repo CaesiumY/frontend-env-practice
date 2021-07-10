1f556a1a6fc3a13d010fc95563fed41c61e57daa
caesiumy <dbs2636@gmail.com>
2021-07-10 23:33:29 +0900

    Start 3-2 mission

---
+++ 3-lint/2-prettier/src/app.js
@@ -0,0 +1,13 @@
+/**
+ * TODO: Prettier가 스타일을 수정합니다.
+ */
+
+
+import MainController from './controllers/MainController.js'
+
+import './app.scss'
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+new MainController(); 
+})
---
+++ 3-lint/2-prettier/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production"
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
+  ]
+};
