405abe80879e928472788027c18ce82f427d94c7
caesiumy <dbs2636@gmail.com>
2021-07-23 01:23:34 +0900

    ch5: Add Asset Modules

--- 5-webpack5.0/src/index.js
+++ 5-webpack5.0/src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import siro from './siro.png'
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,11 @@ function component() {
   // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
   element.innerHTML = _.join(["Hello", "webpack", "!"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = siro
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- 5-webpack5.0/webpack.config.js
+++ 5-webpack5.0/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: { bundle: "./src/index.js" },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    // assetModuleFilename: "images/[hash][ext][query]", // asset 타입을 통해 만들어진 파일들을 모두 모아두는 곳을 설정합니다.
   },
   module: {
     rules: [
@@ -12,6 +14,43 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource", // 기존의 file-loader를 대체합니다.
+        generator: {
+          filename: "static/[hash][ext][query]", // 생성되는 파일의 경로 및 이름을 정합니다.
+        },
+      },
+      {
+        test: /\.html/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/[hash][ext][query]",
+        },
+      },
+      {
+        test: /.svg$/i, // 모든 svg 파일들은 data URI로 번들됩니다. 용량이 작으면 data URI가 효율이 좋습니다.
+        type: "asset/inline", // 기존의 url-loader를 대체합니다.
+        generator: {
+          filename: "static/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.txt/, // 파일 그대로 번들됩니다.
+       type: 'asset/source', // 기존의 raw-loader를 대체합니다.
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset", // 8kb를 경계로 resource와 inline 중 알아서 선택합니다.
+        generator: {
+          filename: "static/[hash][ext][query]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024 // 함수를 통해 4kb로 바꿔줄 수도 있습니다.
+          }
+        }
+       }
     ],
   },
 };
