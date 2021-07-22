a4063ac0c38fad2e0c0de584d19135fa93942073
caesiumy <dbs2636@gmail.com>
2021-07-23 00:17:43 +0900

    ch5: Add loaders for css

--- 5-webpack5.0/src/index.js
+++ 5-webpack5.0/src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash'
+import _ from "lodash";
+import "./style.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
-  element.innerHTML = _.join(['Hello', 'webpack', '!'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack", "!"], " ");
+  element.classList.add("hello");
 
   return element;
 }
