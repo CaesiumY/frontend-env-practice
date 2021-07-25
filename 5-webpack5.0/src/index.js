412faf2e1fa4a104bfa7a34b4e0b96310e4dbbe0
caesiumy <dbs2636@gmail.com>
2021-07-25 20:02:35 +0900

    ch5: Add Output Management

--- 5-webpack5.0/src/index.js
+++ 5-webpack5.0/src/index.js
@@ -1,18 +1,23 @@
 import _ from "lodash";
 import "./style.css";
-import siro from './siro.png'
+import siro from "./siro.png";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
   element.innerHTML = _.join(["Hello", "webpack", "!"], " ");
   element.classList.add("hello");
 
   const myIcon = new Image();
-  myIcon.src = siro
+  myIcon.src = siro;
+
+  btn.onclick = printMe;
 
   element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
--- 5-webpack5.0/webpack.config.js
+++ 5-webpack5.0/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: { bundle: "./src/index.js" },
+  entry: { index: "./src/index.js", print: "./src/print.js" },
   output: {
-    filename: "[name].js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     // assetModuleFilename: "images/[hash][ext][query]", // asset 타입을 통해 만들어진 파일들을 모두 모아두는 곳을 설정합니다.
+    clean: true, // dist의 폴더 내용을 제거하고 다시 설치
   },
   module: {
     rules: [
@@ -37,20 +39,25 @@ module.exports = {
       },
       {
         test: /\.txt/, // 파일 그대로 번들됩니다.
-       type: 'asset/source', // 기존의 raw-loader를 대체합니다.
+        type: "asset/source", // 기존의 raw-loader를 대체합니다.
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset", // 8kb를 경계로 resource와 inline 중 알아서 선택합니다.
+        type: "asset", // 8kb를 경계로 resource와 inline 중 알아서 선택합니다. 가능하면 이게 편합니다.
         generator: {
           filename: "static/[hash][ext][query]",
         },
         parser: {
           dataUrlCondition: {
-            maxSize: 4 * 1024 // 함수를 통해 4kb로 바꿔줄 수도 있습니다.
-          }
-        }
-       }
+            maxSize: 4 * 1024, // 함수를 통해 4kb로 바꿔줄 수도 있습니다.
+          },
+        },
+      },
     ],
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Output Management!1",
+    }), // html 파일을 동적으로 다시 생성
+  ],
 };
